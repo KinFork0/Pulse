302afe443b870c5fa299b239a0a96c4103fd574d
KinFork <Hacker048228@mail.ru>
2025-02-09 22:34:46 +0500

    created sections and tabs

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,8 @@
 $(document).ready(function () {
     $('.carousel__wrapper').slick({
         speed: 1200,
-        prevArrow: '<button type="button" class="slick-prev"><img src="../arrow_left.svg" alt="arrow"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="../arrow_right.svg" alt="arrow"></button>',
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/carousel/arrow_left.svg" alt="arrow"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/carousel/arrow_right.svg" alt="arrow"></button>',
         responsive: [
             {
                 breakpoint: 992,
@@ -13,4 +13,23 @@ $(document).ready(function () {
             }
         ]
     });
+
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function () {
+        $(this)
+            .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+            .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+    });
+
+    function toggleSlide(item) {
+        $(item).each(function (i) {
+            $(this).on('click', function (e) {
+                e.preventDefault();
+                $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+                $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+            });
+        });
+    };
+
+    toggleSlide('.catalog-item__link');
+    toggleSlide('.catalog-item__back');
 });
